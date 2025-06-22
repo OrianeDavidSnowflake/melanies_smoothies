0096b25f1b28339ed39cec005a52592c97ade441
OrianeDavidSnowflake <oriane.david@wavestone.com>
2025-06-23 01:12:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched
 
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
 cnx = st.connection("snowflake")
 
 session = cnx.session() 
