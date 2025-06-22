840efbcc60cd4c151eab2379df513131823dcc14
OrianeDavidSnowflake <oriane.david@wavestone.com>
2025-06-23 01:45:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,9 +43,9 @@ if ingredient_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fr, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fr,' is ', search_on, '.')
    
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
+        #smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ search_on)
         #st.text(smoothiefroot_response.json())
-        st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
+        #st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ing_str + """','""" + name_on_order + """')"""
